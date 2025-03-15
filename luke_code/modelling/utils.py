72b1adfb181174ca362317133a2937fcae75b804
lwilsen <lwilsen@umass.edu>
2025-03-15 11:47:39 -0600

    added year training and test

--- luke_code/modelling/utils.py
+++ luke_code/modelling/utils.py
@@ -119,3 +119,14 @@ def piped_traineval(
     model_accuracy_titanic_compare_piped[model_key] = accuracy
 
     return accuracy
+
+def df_train_test(df, target_col, test_size=0.2, random_state=42):
+    '''This function takes in a dataframe, a target column, and a test size, and returns the training and testing data'''
+    from sklearn.model_selection import train_test_split
+
+    X = df.drop(target_col, axis=1)
+    y = df[target_col]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+
+    return X_train, X_test, y_train, y_test
